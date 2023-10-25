9802a0e35f1694ed17a5e0755a251240468e6b0d
Viviana <yertmaylo15@gmail.com>
2023-10-24 19:40:49 -0500

    v06 se config. firebase y registro de un objeto

--- src/pagina/AppForm.js
+++ src/pagina/AppForm.js
@@ -1,15 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { db } from './firebase';
+import { addDoc, collection } from 'firebase/firestore';
 
 const AppForm = () => {
-    return (
-        <div style={{background:"orange",padding:"10px",textAlign:"center"}}>
-        <h1> AppForm.js</h1>
-        <p>Nombre</p>
-        <p>Edad</p>
-        <p>Genero</p>
-        <p>Guardar</p>
-        </div> 
-    )
+
+  ////////////////GUARDAR O ACTUALIZAR////////////////
+  const camposRegistro = { nombre: "", edad:"",genero:""}
+  const [objeto, setObjeto] = useState(camposRegistro);
+  const handleSubmit  =(e)=> { //manejador de submit
+    e.preventDefault();
+
+    try {
+      if(db){
+        addDoc(collection(db,'persona'), objeto);
+        console.log("se guardo con exito"); 
+      }
+    }catch (error){
+      console.error();
+    }
+  }
+  return (
+    <div style ={{background:"orange",padding:"10px", textAlign:"center"}}>
+      <form onSubmit={ handleSubmit }>
+      <h1>AppForm.js</h1>
+      <input type='text'placeholder='Nombre...'/>
+      <input type='text'placeholder='Apellido...'/>
+      <select>
+        <option value="">Seleccione...</option>
+        <option value="M">Masculino...</option>
+        <option value="F">Femenino...</option>
+        </select>
+        <button>
+          GUARDAR/ACTUALIZAR
+        </button>
+      
+      </form>
+    </div>
+  )
 }
 
 export default AppForm
--- src/pagina/firebase.js
+++ src/pagina/firebase.js
@@ -3,6 +3,10 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
+import{getStorage} from "firebase/storage";
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBKN33FhzG4UhaZJITnm3YBvL1zmwUM648",
@@ -15,3 +19,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
