e01c8bb895f6ca2d6e547bac4a16a47448e91763
Viviana <yertmaylo15@gmail.com>
2023-10-25 19:21:14 -0500

    v07.Validacion, Lectura a BD

--- src/pagina/AppForm.js
+++ src/pagina/AppForm.js
@@ -11,25 +11,53 @@ const AppForm = () => {
     e.preventDefault();
 
     try {
-      if(db){
-        addDoc(collection(db,'persona'), objeto);
+      if(validarForm()){
+        addDoc (collection(db, 'persona'), objeto);
         console.log("se guardo con exito"); 
+      }else{
+        console.log("No se guardo");
       }
     }catch (error){
       console.error();
     }
   }
+
+  // manejador del estado de cambios
+
+  const handleStatusChange =(e) => {
+    const {name, value} = e.target;
+    setObjeto({...objeto,[name]:value});
+    console.log(objeto,value);
+  }
+  //Validacion
+  const validarForm = () => {
+    if(objeto.nombre == ''){
+      alert("Escriba nombres...");
+      return false
+    }
+    return true;
+  }
+  
   return (
     <div style ={{background:"orange",padding:"10px", textAlign:"center"}}>
       <form onSubmit={ handleSubmit }>
       <h1>AppForm.js</h1>
-      <input type='text'placeholder='Nombre...'/>
-      <input type='text'placeholder='Apellido...'/>
-      <select>
+
+      <input onChange={handleStatusChange} 
+      value={objeto.nombre} name='nombre'
+       type='text'placeholder='Nombre...'/> <br/>
+
+      <input onChange={handleStatusChange} 
+      value={objeto.edad} name='edad'
+       type='text'placeholder='Edad...'/> <br/>
+    
+      
+      <select onChange={handleStatusChange} 
+        name='genero'> 
         <option value="">Seleccione...</option>
-        <option value="M">Masculino...</option>
+        <option value= "M">Masculino...</option>
         <option value="F">Femenino...</option>
-        </select>
+        </select><br/>
         <button>
           GUARDAR/ACTUALIZAR
         </button>
